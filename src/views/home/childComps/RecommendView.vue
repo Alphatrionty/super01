8e8ac5bf0de6501d089a5577878e3623f8323963
Alphatrionty <1379844203@qq.com>
2020-05-03 19:01:42 +0800

    tabcontrol封装

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RecommendView",
+    props: {
+       recommends: {
+         type: Array,
+         default() {
+           return []
+         }
+       }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+
+    padding: 4px 0 12px;
+    border-bottom: 8px solid #eeeeee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    width: 76px;
+    height: 75px;
+    margin-bottom: 4px;
+  }
+</style>
